48386c760a493e21e966a3f2e6e15f32af14e7c7
Mumtahina Nur <mumtahinanur9@gmail.com>
2026-02-05 19:46:00 +0600

    write a function to fetch the weather data and parse it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./styles.css";
 //import image from "./"
 
+const API_KEY = 'ee892c26f0034d78d2c3832d2e9367af';
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
+
+async function getWeatherData(location) {
+  try {
+    const response = await fetch(API_URL + `&appid=${API_KEY}` + `&q=${location}`);
+    const data = await response.json();
+    console.log(data);
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+getWeatherData('dhaka');
